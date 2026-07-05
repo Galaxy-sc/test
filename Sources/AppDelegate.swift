956af7a1adce4727682d0616c1a84a828b347357
Meysam Bal-afkan <94435150+Galaxy-sc@users.noreply.github.com>
2026-07-05 15:00:02 +0330

    Remove Frida Gadget loading logic
    Removed Frida Gadget loading code from AppDelegate.

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Darwin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,20 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // --- لود کردن فریدا از داخل فریم‌ورک مبدل ---
-        if let frameworksPath = Bundle.main.privateFrameworksPath {
-            // آدرس آپدیت شد:
-            let gadgetPath = frameworksPath + "/FridaGadget.framework/FridaGadget"
-            let handle = dlopen(gadgetPath, RTLD_NOW)
-            
-            if handle != nil {
-                print("[+] Frida Gadget Framework Loaded Successfully!")
-            } else {
-                print("[-] Failed to load Frida Gadget Framework.")
-            }
-        }
-        // ---------------------------------------------
-
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .white
         window?.rootViewController = ViewController()
